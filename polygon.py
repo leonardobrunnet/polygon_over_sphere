b412ab0f5f394033221a31ea7f2564a6390daeea
leonardobrunnet <leon@if.ufrgs.br>
2022-03-29 21:31:48 -0300

    - Implemented variable dt - Relax kb after division

--- polygon.py
+++ polygon.py
@@ -8,6 +8,7 @@ import torch
 import numpy as np
 from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -54,7 +55,6 @@ def generate_polar_circle(r,Rs,num_part):
 
 def generate_circle_at_theta_phi(num_part,r,Rs,theta,phi):
     delta = phi + torch.tensor(np.pi)/2
-    #print(theta)
     Z = generate_polar_circle(r,Rs,num_part)
     Z = rotate_around_y(Z,delta)
     Z = rotate_around_z(Z,theta)
@@ -73,7 +73,6 @@ def rotate_around_y(X,phi):
     return Y
     
 def rotate_around_z(X,theta):
-    #print(theta)
     Y = torch.zeros_like(X)
     Y[:,0] = X[:,0]*torch.cos(theta) - X[:,1]*torch.sin(theta)
     Y[:,1] = X[:,1]*torch.cos(theta) + X[:,0]*torch.sin(theta)
@@ -109,7 +108,6 @@ def make_3d_image(poly_list):
     for i in poly_list:
         xs,ys,zs=generate_image_points(i.X)
         xs,ys,zs=xs.to("cpu"),ys.to("cpu"),zs.to("cpu")
-        #print(xs,ys,zs)
         ax.scatter(xs,ys,zs)
     ax.view_init(90,0)
 #plt.show()
@@ -123,8 +121,6 @@ def make_3d_image_cm(poly_list):
 #     ax = fig.add_subplot(projection='3d')
     for i in poly_list:
         xs,ys,zs=i.cm.cpu()
-        print(xs,ys,zs)
-        #print(xs,ys,zs)
         ax.scatter(xs,ys,zs)
     #ax.view_init(np.pi/4,np.pi/4)
 #plt.show()
@@ -146,7 +142,6 @@ def neighbors(poly_list,r):
     z=torch.tensor(np.arange(1,lp+1),device=device)
     for i in poly_list:
         y = list(map(int,((D[i.ident]*z)).tolist()))
-        #print(y)
         y = list(filter(lambda num: num != 0, y[i.ident:]))
         y = list(i-1 for i in y)
         i.neighbor_list=y
@@ -158,7 +153,6 @@ def neighbors(poly_list,r):
 
 
 def dist_mat_square(X):
-    #print(X.type())
     return torch.sqrt(torch.sum((X[:,None,:]-X[None,:,:])**2,axis=2))
 
 
@@ -216,21 +210,19 @@ def force_field_intra_polygon(X,R,zero_tensor,Req,Frep):
 class polygon:
     Req = 1.
     ks = 10
-    kb = 30
     R0 = 1.2
-    Frep = 10.
-    Fadh = 1.
-    inter_Fadh = 0.
+    Frep = 20.
+    Fadh = 8.
     mu = 1.
     
-    def __init__(self,ident,X,dt):
-        self.X = X.clone().detach()
+    def __init__(self,ident,X):
+        self.X = X.clone().detach().to(device)
         self.ident = ident
         self.num_part = len(X)
         self.Force = torch.zeros(self.num_part,3,device=device)
-        self.dt = dt
         self.neighbor_list = []
-        
+        self.maxforce = 0
+        self.kb = kb*self.num_part
         
     def perimeter_and_self_repulsion_forces(self):
         Xp = torch.cat((self.X[:,:],self.X[None,0,:]),0)#adding first particle postion to the end
@@ -242,23 +234,14 @@ class polygon:
         f_vec_d = self.Xd * torch.div(fnormd,torch.norm(self.Xd,dim=1))[:,None]
         f_vec_e = self.Xe * torch.div(fnorme,torch.norm(self.Xe,dim=1))[:,None]  
         self.Force = f_vec_d + f_vec_e
-        # if t > t_ana and self.ident == 0:
-        #     print(t)
-        #     for j in range(self.num_part):
-        #         print(self.X[j],self.Force[j])
-        #     print("\n")
-        #print(self.Force)
         #Repulsion if not first neighbors when too close
         R = dist_mat_square(self.X)
-        z = torch.ones(self.num_part)
+        z = torch.ones(self.num_part,device=device)
         tridiag = (torch.diag(z[:-1],1)+torch.diag(z)+torch.diag(z[:-1],-1))*1000 # large tridiagonal matrix
         tridiag = tridiag.to(device)
         R += tridiag #summing a large tridiagonal to avoid auto zero distance and already calculated neighbors
         zero_tensor = torch.zeros(self.num_part,self.num_part,device=device)
         FF = force_field_intra_polygon(self.X,R,zero_tensor,self.Req,self.Frep)
-#         if t > 1.0199:
-#             for j in range(len(self.X)):
-#                 print(self.X[j],FF[j])
         self.Force += FF
         
         return
@@ -277,21 +260,18 @@ class polygon:
         #forces on focus site and first neighbor should sum zero
         self.Force[index_d] +=  self.kb*self.angle[:,None]*X_perp_d/(2.*torch.cos(self.angle[:,None]/2.)) 
         self.Force[index_e] +=  self.kb*self.angle[:,None]*X_perp_e/(2.*torch.cos(self.angle[:,None]/2.))
-        # if t > t_ana and self.ident == 0:
-        #     for j in range(self.num_part) :
-        #         print(self.X[j],self.Force[j])
-        #     print("\n")
-
+        self.maxforce=max(torch.norm(self.Force,dim=1)).item()
         return 
     
     def move_cell_particles(self):
-        dx = self.mu*self.Force*self.dt
+        dx = self.mu*self.Force*dt
+
         self.X += dx
         #back to the surface
         R,theta,phi = convert_cartesian_tensor_to_spherical(self.X)
         R=torch.ones(self.num_part,device=device)*Rs0
         self.X=convert_spherical_tensor_to_cartesian(R,theta,phi)
-#        print(self.X.type())
+
 
         return
     
@@ -301,11 +281,9 @@ class polygon:
     
     def center_of_mass(self):
         self.cm = torch.sum(self.X,0)/len(self.X)
-        #print(self.cm)
         return
     
     def inter_polygon_forces(self):
-        #print(self.neighbor_list)
         for i in self.neighbor_list :
             R = distmat_square_inter_polygons(self.X,poly_list[i].X)
             zero_tensor=torch.zeros(self.num_part,self.num_part,device=device)
@@ -329,8 +307,6 @@ def division(poly_list,r,division_axis):
     num_part = len(poly_list[mother_cell].X)
     Xcm_mother = poly_list[mother_cell].cm
     R,theta,phi = convert_cartesian_to_spherical(Xcm_mother)
-    #R,theta,phi = R_theta_phi_cm[0],R_theta_phi_cm[1],R_theta_phi_cm[2]
-    print(R,theta,phi)
     dphi=torch.arcsin(1.1*r/(2*R))
     X_mother=generate_circle_at_theta_phi(num_part,r/2.2,R,division_axis*pi/2,dphi) #at zenit
     X_mother=rotate_from_zenite_to_theta_phi(X_mother,theta,phi)
@@ -338,7 +314,7 @@ def division(poly_list,r,division_axis):
     X_daughter=generate_circle_at_theta_phi(num_part,r/2.2,R,division_axis*pi/2,-dphi) #at zenit
     X_daughter = rotate_from_zenite_to_theta_phi(X_daughter,theta,phi)
     new_index=len(poly_list)
-    poly_list.append(polygon(new_index,X_daughter,dt))
+    poly_list.append(polygon(new_index,X_daughter))
     return
 
 
@@ -346,9 +322,13 @@ def division(poly_list,r,division_axis):
 
 
 def init():
+    global t, kb, Rs0,dt
     device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device="cpu"
+    print("device=",device)
     dt = 0.004
-    num_part = 15             #number of particles per polygon
+    kb = 3.
+    num_part = 30        #number of particles per polygon
     Req = 1        #equilibrium distance between polygon particles
     r=torch.tensor(num_part*Req/(2*np.pi)) #polygon "radius"
     Rs0 = 10*r       #sphere radius
@@ -363,67 +343,55 @@ def init():
     poly_list = []
     X=generate_polar_circle(r,Rs0,num_part) #circle centered at zenith
     X = X.to(device)
-    poly_list.append(polygon(0,X,dt)) #first cell
-    return poly_list,dt,r,Rs0
+    poly_list.append(polygon(0,X)) #first cell
+    return poly_list,r
         
 
 
 # In[ ]:
-
+                     
 
 #main program
-global t, t_ana, Rs0
-poly_list,dt,r,Rs0= init()
+poly_list,r= init()
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 #make_3d_image(poly_list)
 dt0=dt
-t,tf,division_time,finish_division= 0.,23.9, 2.0,20.1
-t_ana = tf
-int_division_time = int(division_time/dt)
-#poly_list[0].X[0]=torch.tensor([0.0,0.0,7.9179])
-#print(poly_list[0].X)
-
-count = 0
-division_flag = 0
-relax_division_counter =0
-time_limit_relax = 100
+t,tf,division_time,finish_division= 0.,40., 10.0,35.
+next_division=division_time
 division_axis = 0
+intt=0
+time0=time.time()
 while t < tf:
     t+=dt
-    #print(dt)
-    if division_flag == 1 and relax_division_counter < time_limit_relax:
-        relax_division_counter+=1
-        #if relax_division_counter == 90 :
-            #list(map(lambda i:i.center_of_mass(),poly_list))
-            #make_3d_image(poly_list)
-            #exit()
-    else:
-        division_flag = 0
-        relax_division_counter = 0
-        dt = dt0
         
     list(map(lambda i:i.zero_forces(),poly_list))
     list(map(lambda i:i.perimeter_and_self_repulsion_forces(),poly_list))
-    list(map(lambda i:i.bending_force(),poly_list))
+    if kb > 0: list(map(lambda i:i.bending_force(),poly_list))
     list(map(lambda i:i.center_of_mass(),poly_list))
     neighbors(poly_list,r)
     list(map(lambda i:i.inter_polygon_forces(),poly_list))
-    
+    maxforce_list=[]
+    for i in poly_list:
+        maxforce_list.append(i.maxforce)
+    #print(maxforce_list)
+    fmax=max(maxforce_list)
+    #print(fmax)
+    dt=min(0.004/fmax,0.01)
+    if int(t)>intt:
+        print(t,dt)
+        intt=int(t)
+    if t > finish_division and t < finish_division+dt:
+        print("Finished dividing. Reseting kb to lower value")
+        kb=kb/10
     list(map(lambda i:i.move_cell_particles(),poly_list))
-    #print(poly_list[0].X.type())
-    #make_3d_image(poly_list)
-    if division_flag == 0 : count+=1
-    #print(count)
-    if count%int_division_time == 0 and t<finish_division:
+    if t>next_division and t<finish_division:
         division_axis=np.mod(division_axis,2) #division once in x another in y
         division(poly_list,r,division_axis)
         division_axis+=1
-        #
-        dt=dt/5.
-        division_flag=1
-        count+=1
+        next_division+=division_time
 #     plt.show()
+print(time.time()-time0)
 list(map(lambda i:i.center_of_mass(),poly_list))
 #make_3d_image_cm(poly_list)
 make_3d_image(poly_list)
